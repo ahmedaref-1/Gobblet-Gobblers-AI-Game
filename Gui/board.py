16d895dcf70e66f2e442d47abb206ce211404a16
bahaa sherif <104047270+bahaasherif@users.noreply.github.com>
2023-12-23 03:01:11 +0200

    board.py modyfing

--- Gui/board.py
+++ Gui/board.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_MainWindow(object):
+class BoardWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1083, 838)
@@ -463,7 +463,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
+    ui = BoardWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
