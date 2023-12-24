cd1c2aba3e9fccb89140e0dfe38df95cb09a2144
Ahmed Aref <ahmed.aref.omaira@gmail.com>
2023-12-24 18:18:10 +0200

    Update Gobblet.py
    The is possible move API also

--- Gobblet.py
+++ Gobblet.py
@@ -66,7 +66,7 @@ class Gobblet:
         """
         # Check if the specified board item position is empty.
         # If empty, the current Gobblet can be moved to the specified board item position; otherwise, it cannot.
-        return board_item.IsEmpty() or self.Size > board_item.OnTopGobbletSize
+        return board_item.IsEmpty() or((not board_item.IsFull()) and (self.Size > board_item.OnTopGobbletSize))
 
     def GetGobbletOwnedPositionIndex(self):
         """
