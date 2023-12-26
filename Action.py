95a3357953c10161b055be30dab7d2dc282d9db8
Ahmed Aref <ahmed.aref.omaira@gmail.com>
2023-12-26 02:35:52 +0200

    Update Action.py
    1.solved all Errors and Warnings in the file
    2.Modified the naming conventions to follow the python convention

--- Action.py
+++ Action.py
@@ -1,7 +1,8 @@
-from typing import List
-import BoardItem
 import Gobblet
+import BoardItem
+
+
 class MoveID:
-    def __init__(self, CurrentGobblet: Gobblet, NextPossibleLocation):
-        self.CurrentGobblet = CurrentGobblet
-        self.next = NextPossibleLocation
+    def __init__(self, current_gobblet: Gobblet, next_possible_location: BoardItem):
+        self.CurrentGobblet = current_gobblet
+        self.next = next_possible_location
